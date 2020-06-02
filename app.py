fe458d01aeb91f13bf934b35396a369f2843c0ca
CatButtes <catbuttes@outlook.com>
2020-06-02 20:50:52 +0100

    Add some logging and basic error handling

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 import CommunityBot
 import os
+import yaml
+import logging
+import logging.config
 
 
 def main():
+
+    with open("logging.yml", 'rt') as f:
+        logging_config = yaml.safe_load(f.read())
+        logging.config.dictConfig(logging_config)
 
     config = dict()
     config["bot_token"] = os.environ['BOT_TOKEN']
@@ -13,4 +20,5 @@ def main():
 
 
 if __name__ == "__main__":
+
     main()
