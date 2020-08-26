98fef03afe6f904d3150d52735813a546eb5baa4
CatButtes <catbuttes@outlook.com>
2020-08-26 14:26:01 +0100

    Strip 'v' when deploying

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -5,6 +5,6 @@ mkdir ~/.ssh
 ansible-vault view --vault-password-file=~/.vault_pass.txt ansible/ssh/ansible_key > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 
-BOT_VERSION=$(echo "$BOT_VERSION_RAW" | sed -e 's,.*/\(.*\),\1,')
+BOT_VERSION=$(echo "$BOT_VERSION_RAW" | sed -e 's,.*/\(.*\),\1,' | sed -e 's/^v//')
 
 ansible-playbook -e "communitybot_version=$BOT_VERSION" -e "DISCORD_BOT_TOKEN=$DISCORD_BOT_TOKEN" --vault-password-file ~/.vault_pass.txt -i ansible/hosts/hosts.yml ansible/deploy.yml --become
